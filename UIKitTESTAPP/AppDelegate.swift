8c718100dcca616dd80da95126a9f9d30f538101
sikwan <yysskk99999@naver.com>
2024-09-26 19:02:16 +0900

    first commit

---
+++ UIKitTESTAPP/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  UIKitTESTAPP
+//
+//  Created by 양시관 on 9/26/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ UIKitTESTAPP/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UIKitTESTAPP
+//
+//  Created by 양시관 on 9/26/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
